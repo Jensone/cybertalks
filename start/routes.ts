f6b294075fb07995719c46810ba2299d3239826e
Jensone <hello@jensone.com>
2024-06-14 10:44:22 +0000

    🔧 corriger(routes.ts) : importer la dépendance manquante { request } depuis 'http' 🚀 fonctionnalité(routes.ts) : ajouter des routes pour créer et enregistrer un événement

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router' // Routeur de Adonis
+import { request } from 'http'
 
 // Importation des controllers
 const EventsController = () => import('#controllers/events_controller')
@@ -20,6 +21,23 @@ router.get('/events', [EventsController, 'all']).as('events')
 
 // Routes pour afficher un événement
 router.get('/event/:id', [EventsController, 'show']).as('event')
+
+// Route pour créer un événement
+router.get('events/create', async ({ view }) => {
+  return view.render('events/create')
+}).as('create')
+
+// Route pour enregistrer un événement
+router.post('/event/store', async ({ request, response, session }) => {
+    const data = request.toJSON().body
+    console.log(data)
+    // Add flash message
+    session.flash('notification', {
+      type: 'success',
+      message: 'Thanks for adding a new event!'
+    })
+    response.redirect('/events')
+}).as('store')
 
 // Route de la page de contact
 router.on('/contact').render('pages/contact').as('contact')
